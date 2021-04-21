96d065093e2c3afa6e0cfde57394d94df319f4dc
Zaki <zaki@Zakis-MBP.phub.net.cable.rogers.com>
2021-04-21 13:58:39 -0400

    add Boundary API

--- ongeza/lib/ongeza/boundary/service.ex
+++ ongeza/lib/ongeza/boundary/service.ex
@@ -1,13 +1,11 @@
 defmodule Ongeza.Boundary.Service do
   alias Ongeza.Core.Counter
 
-  def listen(count) do
-    receive do
-      :inc -> Counter.add(count) #handle_cast
-      {:state, from} -> #handle_call
-        send(from, count)
-        count
-    end
+  def start(count) do
+    #callback into init
+    spawn( fn ->
+      run(count)
+    end)
   end
 
   def run(count) do
@@ -16,10 +14,37 @@ defmodule Ongeza.Boundary.Service do
     |> run()
   end
 
-  def start(count) do
-    #callback into init
-    spawn( fn ->
-      run(count)
-    end)
+  def listen(count) do
+    receive do
+      :inc -> Counter.add(count) #handle_cast
+
+      {:state, from} -> #handle_call
+        send(from, count)
+        count #state
+    end
+  end
+
+  # request increment (Process A)
+  def inc(counter) do
+    send(counter, :inc)
+  end
+
+  # ask for and receive state (Process B)
+  def state(counter) do
+
+    send(counter, {:state, self})
+
+    receive do
+      counter -> counter
+    end
+
   end
 end
+
+
+# alias - just the trailing word e.g. Ongeza.Core.Counter -> Counter
+# import -  making all the functions available as if they were declared publicly
+# use -  call the __USING__ macro (code that writes code)
+
+# SERVICE (counter) --> CLIENT (iex)
+#      186                  183
