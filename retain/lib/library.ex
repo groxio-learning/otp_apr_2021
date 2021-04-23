bbc040145482d65fe29c4d5650efabe2e3a8199b
Zaki <zaki@Zakis-MBP.phub.net.cable.rogers.com>
2021-04-23 09:06:18 -0400

    zaki's text

--- retain/lib/library.ex
+++ retain/lib/library.ex
@@ -1,23 +1,32 @@
 defmodule Retain.Library do
   ~w[bruce tracey mo zaki christine]
-  
+
   def card(item) when is_atom(item), do: item |> to_string |> card
-  
+
   def card("bruce") do
     %{
       text: """
-            We the People of the United States, 
-            in Order to form a more perfect Union, 
-            establish Justice, insure domestic Tranquility, 
-            provide for the common defence, 
-            promote the general Welfare, 
-            and secure the Blessings of Liberty 
-            to ourselves and our Posterity, 
-            do ordain and establish this Constitution 
+            We the People of the United States,
+            in Order to form a more perfect Union,
+            establish Justice, insure domestic Tranquility,
+            provide for the common defence,
+            promote the general Welfare,
+            and secure the Blessings of Liberty
+            to ourselves and our Posterity,
+            do ordain and establish this Constitution
             for the United States of America.
-            """, 
+            """,
       steps: 8
     }
   end
-  
+
+  def card("zaki") do
+    %{
+      text: """
+            software is eating the world but AI will eat software
+            """
+    }
+
+  end
+
 end
