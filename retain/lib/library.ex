916185284a8aa351e706df8a4a61a6cad09e867f
mohammed hindawash <djslakivich@gmail.com>
2021-04-23 09:07:37 -0400

    My sentence homework added in library.ex

--- retain/lib/library.ex
+++ retain/lib/library.ex
@@ -1,23 +1,32 @@
 defmodule Retain.Library do
   ~w[bruce tracey mo zaki christine]
-  
+
   def card(item) when is_atom(item), do: item |> to_string |> card
-  
+
   def card("bruce") do
     %{
       text: """
-            We the People of the United States, 
-            in Order to form a more perfect Union, 
-            establish Justice, insure domestic Tranquility, 
-            provide for the common defence, 
-            promote the general Welfare, 
-            and secure the Blessings of Liberty 
-            to ourselves and our Posterity, 
-            do ordain and establish this Constitution 
+            We the People of the United States,
+            in Order to form a more perfect Union,
+            establish Justice, insure domestic Tranquility,
+            provide for the common defence,
+            promote the general Welfare,
+            and secure the Blessings of Liberty
+            to ourselves and our Posterity,
+            do ordain and establish this Constitution
             for the United States of America.
-            """, 
+            """,
       steps: 8
     }
   end
-  
+
+  def card("moe") do
+    %{
+      text: """
+            What runs but never walks, what has a mouth but never talks
+            """
+      steps: 4
+    }
+  end
+
 end
--- retain/lib/retain/application.ex
+++ retain/lib/retain/application.ex
@@ -1,19 +1,12 @@
 defmodule Retain.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: Retain.Worker.start_link(arg)
       {Retain, {"Hello", 5}}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Retain.Supervisor]
     Supervisor.start_link(children, opts)
   end
